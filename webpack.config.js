9d09498bed61c8555c847d5279f6ffd972bfe10d
David Myška <miskith@gmail.com>
2021-04-01 10:19:24 +0200

    Update dependencies & small cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
 		],
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js"]
+		extensions: ['.tsx', '.ts', '.js']
 	},
 	devServer: {
 		contentBase: path.join(__dirname, 'example'),
